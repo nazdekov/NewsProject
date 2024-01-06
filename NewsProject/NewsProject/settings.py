06a092b8c5aa199b1ed6e1b91110aa0a5e37dfaf
NewsProject <nazdekov@gmail.com>
2024-01-07 02:45:30 +0500

    07.01.2024 02:15 Commit - sha256 Function.

--- NewsProject/NewsProject/settings.py
+++ NewsProject/NewsProject/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY")
-
+GITHUB_WEBHOOK_KEY = env("GITHUB_WEBHOOK_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 
 # Для проверки своей страницы 404, конфигурация такая:
--- NewsProject/main/views.py
+++ NewsProject/main/views.py
@@ -10,30 +10,30 @@ import hashlib
 import hmac
 from django.http import HttpResponseServerError
 
-#
-# def verify_signature(payload_body, secret_token, signature_header):
-#     """Verify that the payload was sent from GitHub by validating SHA256.
-#
-#     Raise and return 403 if not authorized.
-#
-#     Args:
-#         payload_body: original request body to verify (request.body())
-#         secret_token: GitHub app webhook token (WEBHOOK_SECRET)
-#         signature_header: header received from GitHub (x-hub-signature-256)
-#     """
-#     if not signature_header:
-#         return HttpResponseServerError("x-hub-signature-256 header is missing!", status=403)
-#     hash_object = hmac.new(secret_token.encode('utf-8'), msg=payload_body, digestmod=hashlib.sha256)
-#     expected_signature = "sha256=" + hash_object.hexdigest()
-#     if not hmac.compare_digest(expected_signature, signature_header):
-#         return HttpResponseServerError("Request signatures didn't match!", status=403)
+
+def verify_signature(payload_body, secret_token, signature_header):
+    """Verify that the payload was sent from GitHub by validating SHA256.
+
+    Raise and return 403 if not authorized.
+
+    Args:
+        payload_body: original request body to verify (request.body())
+        secret_token: GitHub app webhook token (WEBHOOK_SECRET)
+        signature_header: header received from GitHub (x-hub-signature-256)
+    """
+    if not signature_header:
+        return HttpResponseServerError("x-hub-signature-256 header is missing!", status=403)
+    hash_object = hmac.new(secret_token.encode('utf-8'), msg=payload_body, digestmod=hashlib.sha256)
+    expected_signature = "sha256=" + hash_object.hexdigest()
+    if not hmac.compare_digest(expected_signature, signature_header):
+        return HttpResponseServerError("Request signatures didn't match!", status=403)
 
 
 
 @csrf_exempt
 def update_server(request):
-    # header_signature = request.META.get('HTTP_X_HUB_SIGNATURE')
-    # verify_signature(request.body, settings.GITHUB_WEBHOOK_KEY, header_signature)
+    header_signature = request.META.get('HTTP_X_HUB_SIGNATURE')
+    verify_signature(request.body, settings.GITHUB_WEBHOOK_KEY, header_signature)
     if request.method == "POST":
         local_dir = '/home/nazdekov/NewsProject/'    # local dir это где лежит .git
         repo = git.Repo(local_dir)
